36d5d59d5d0d292d9d637430fd91a7b683ccd962
Maksim Pozhidaev <maxoovka1@gmail.com>
2022-09-23 14:22:18 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const getStream = async () => {
+    try {
+        const stream = await navigator.mediaDevices.getDisplayMedia();
+        return stream;
+    } catch (err) {
+        console.error("error", err);
+    }
+};
+
+const createVideo = stream => {
+    let video = document.createElement('video');
+    video.srcObject = stream;
+    video.autoplay = true;
+
+    let videos = document.querySelector('div.videos');
+    videos.append(video);
+
+    return video;
+};
+
+const form  = document.querySelector('form');
+const input = document.querySelector('form > input');
+
+let stream, video;
+let peer = new Peer();
+
+peer.on('open', async id => {
+    console.log(id);
+
+    stream = await getStream();
+    video  = createVideo(stream);
+});
+
+peer.on('call', call => {
+    call.answer(stream);
+
+    call.on('stream', userStream => {
+        const userVideo = createVideo(userStream);
+    });
+});
+
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    if (input.value == "") {
+        return;
+    }
+
+    let call = peer.call(input.value, stream);
+
+    call.on('stream', userStream => {
+        const userVideo = createVideo(userStream);
+    });
+});
